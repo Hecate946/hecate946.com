e820be2106638ab0545388d9fd43b4be61b14214
Hecate <hecate946@gmail.com>
2026-08-12 13:35:42 -0700

    fix prod projects + contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,9 +13,6 @@ export default defineConfig({
     prefetchAll: false,
     defaultStrategy: 'hover',
   },
-  experimental: {
-    clientPrerender: true,
-  },
   integrations: [
     svelte(),
     sitemap({
